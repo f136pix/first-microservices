7cb41b7cdca009b76d959d0baf8dd3716a2762eb
fi136pix <filipecocinel@gmail.com>
2024-03-14 16:03:31 -0300

    dpeloyed to docker-compose, todo: deploy it to k8s

--- LogService/Docker/entrypoint.sh
+++ LogService/Docker/entrypoint.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "coping env file $APP_ENV"
     cp .env.example .env
 else
     echo "env file exists."
@@ -22,7 +23,4 @@ if [ "$role" = "app" ]; then
 elif [ "$role" = "queue" ]; then
     echo "Running the queue ... "
     php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
-elif [ "$role" = "websocket" ]; then
-    echo "Running the websocket server ... "
-    php artisan websockets:serve
 fi
